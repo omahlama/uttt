cb63a8ea12ee2ce036d8cfbc2521f5b33562af65
Olli Mahlamäki <olli.mahlamaki@reaktor.fi>
2014-12-12 23:36:47 +0200

    4 threads

--- src/js/ai.js
+++ src/js/ai.js
@@ -1,5 +1,4 @@
 var uttt = require("./uttt.js")
-var SimpleWorker = require("../lib/simple-worker.js").SimpleWorker
 
 function ai(heuristic, searchDepth) {
 
@@ -13,30 +12,42 @@ function ai(heuristic, searchDepth) {
 		var bestValue = game.turn === 1 ? Number.NEGATIVE_INFINITY : Number.POSITIVE_INFINITY
 		var bestMove, queue = moves.length, index = 0
 		
-		var worker = new Worker("src/worker.js")
-		worker.onmessage = function(e) {
-			var res = e.data
-		  	var val = res[0], move = res[1]
-			if(game.turn === 1 && val > bestValue || game.turn === -1 && val < bestValue) {
-				bestValue = val
-				bestMove = move
-			} else if(!bestMove) {
-				bestMove = move
+		var workers = [];
+		for(var i=0; i<4; i++) {
+			var worker = new Worker("src/worker.js")
+			worker.onmessage = function(e) {
+				var res = e.data
+			  	var val = res[0], move = res[1]
+				if(game.turn === 1 && val > bestValue || game.turn === -1 && val < bestValue) {
+					bestValue = val
+					bestMove = move
+				} else if(!bestMove) {
+					bestMove = move
+				}
+				queue--;
+				index++
+				this.free = true
+				if(index < moves.length) {
+					doWork()
+				}
+				if(queue === 0) 
+				{
+					cb(bestMove)
+				}			
 			}
-			queue--;
-			index++
-			if(index < moves.length) {
-				doWork()
-			}
-			if(queue === 0) 
-			{
-				cb(bestMove)
-			}			
+			worker.free = true
+			workers.push(worker)
 		}
 
 		function doWork() {
 			var nextGame = uttt.play(game, game.turn, moves[index][0], moves[index][1])
-			worker.postMessage([nextGame, searchDepth-1, moves[index], heuristic.toString()])
+			for(var i=0; i<workers.length; i++) {
+				var worker = workers[i]
+				if(worker.free) {
+					worker.free = false;
+					worker.postMessage([nextGame, searchDepth-1, moves[index], heuristic.toString()])
+				} 
+			}
 		}
 
 		doWork();
